956f46915396152589824520908da827c416ad3b
Sergey Dolin <v-sedoli@microsoft.com>
2021-07-19 09:57:35 +0500

    init

---
+++ app/src/androidTest/java/vsedoli/espressodemo/ExampleInstrumentedTest.java
@@ -0,0 +1,38 @@
+package vsedoli.espressodemo;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+    @Test
+    public void clickButton() {
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.text))
+                .check(matches(withText("Clicked")));
+    }
+
+}
---
+++ app/src/main/java/vsedoli/espressodemo/MainActivity.java
@@ -0,0 +1,19 @@
+package vsedoli.espressodemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(v -> ((TextView)findViewById(R.id.text)).setText("Clicked"));
+    }
+}
